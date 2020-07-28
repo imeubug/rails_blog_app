559b3e77892aaa22ce8038690b56684a9269de02
Jii Eu <jione.eu@gmail.com>
2020-07-28 16:47:50 +0900

    controller and model for Tag/Taggings/tags added & tag feature complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_034347) do
+ActiveRecord::Schema.define(version: 2020_07_28_064537) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author_name"
@@ -28,5 +28,22 @@ ActiveRecord::Schema.define(version: 2020_07_26_034347) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_taggings_on_post_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   add_foreign_key "comments", "posts"
+  add_foreign_key "taggings", "posts"
+  add_foreign_key "taggings", "tags"
 end
